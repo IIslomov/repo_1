4fd04de1d85a51456e49c0fdef092f75439cea0a
IIslomov <mrpocker88@gmail.com>
2023-04-06 10:57:12 +0500

    Update javoblar-12-dars-06.py

--- javoblar-12-dars-06.py
+++ javoblar-12-dars-06.py
@@ -14,4 +14,4 @@ son = int(input("Istalgan butun son kiriting: "))
 
 for n in range(2,11):
     if not (son%n):
-        print(f"{son} soni {n} ga qoldiqsiz bo'linadi")
+        print(f"{son} soni {n} ga githabga qo'shildi")
